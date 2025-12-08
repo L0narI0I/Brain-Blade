26462ee07fc6e67d6b4ba5d922f3595945337057
L0narI0I <acalande@free.fr>
2025-12-08 11:56:14 +0100

    Add files via upload

---
+++ Personnage.java
@@ -0,0 +1,12 @@
+class Personnage {
+    int HPmax = 40;
+    int HPcurrent = HPmax;
+    double defense = 10;
+    double buffDef = 1;
+    int dps = 20;
+    double buffDps = 1;
+    double txCrit = 0.25;
+    double degCrit = 1.5;
+    double buffCrit = 1;
+    Int vitesse = 10;
+}
