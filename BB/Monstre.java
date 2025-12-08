a91408abe89c9b1051a084acbefedaeb989344ed
L0narI0I <acalande@free.fr>
2025-12-08 11:55:29 +0100

    Add files via upload

---
+++ BB/Monstre.java
@@ -0,0 +1,13 @@
+class Monstre {
+    int HPmax;
+    int HPcurrent;
+    int defense;
+    double buffDef = 1;
+    int dps;
+    double buffDps = 1;
+    double txCrit = 0.25;
+    double degCrit = 1.5;
+    double buffCrit = 1;
+    int vitesse;
+    String categorie;
+}
