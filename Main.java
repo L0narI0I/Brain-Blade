33615a13e08b72819ec52971636ab2b9502925d7
L0narI0I <acalande@free.fr>
2025-12-09 21:58:18 +0100

    Légère modification Main.java
    modification toStringAsciiArt avec résolution des problèmes lié
    rajout de la fonction toStringMultipleAsciiArt afin d'afficher plusieurs ascii entre eux (Fonction à tester)

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import extensions.File;
+Cimport extensions.File;
 import extensions.CSVFile;
 class Main extends Program {
 
@@ -6,7 +6,7 @@ class Main extends Program {
     String currentMonster = "";
     Player player = newPlayer(); 
     
-//---------------Constructeur---------------//
+//---------------Constructeur de Classe---------------//
     
     Monstre newMonstre(int HP, int def, int degat, int speed, String type){
         Monstre newMonster = new Monstre();
@@ -26,39 +26,54 @@ class Main extends Program {
 
 //---------------Fonction Utilitaires---------------//
     
-    void afficherAsciiArt(String name_file){
+    String toStringAsciiArt(String name_file){
         File file = newFile("~/ijava2/BB/Assets/"+name_file+".txt");
+        String img;
         while (ready(file)){
-            println(readLine(file));
+            img += readLine(file) + "\n";
         }
+        return img;
     }
+
+    String toStringMultiAsciiArt(String name_file1, String name_file2){
+        File file1 = newFile("~/ijava2/BB/Assets/"+name_file1+".txt");
+        File file2 = newFile("~/ijava2/BB/Assets/"+name_file2+".txt");
+        String img;
+        while (ready(file1) && ready(file2)) {
+            img += readLine(file1) + "\t" + readLine(file2) + "\n";
+        }
+        return img;
+    }      
 
     String controleSaisie(String[] possibilite){
         String saisie;
         boolean valide = false;
         do{
             print("Entrez votre choix");
-            saisie = readInt();
+            saisie = readString();
             for (int i=0 ; i<length(possibilite) ; i++){
                 if(saisie == possibilite[i]){
                     saisie = true;
                 }
             }
-        }while (valide);
+        }while (!valide);
         return saisie;
     }
 
 //---------------Fonction de combat---------------//
     int playerAttack(Player player, Monstre monstre){
+        
         double rd = random(0.85,1.15);
         double dmg = dmgxbuffDmg/(Monster.defense*Monstre.buffDef)*(rd);
     }
 
 //---------------Fonction de Quizz---------------//
 
+//---------------Boucle Principale---------------//
+
     void algorithm() {
         print(CLEAR);
-        afficherAsciiArt("MainScreen");
+        printt(toStringrAsciiArt("MainScreen"));
         while (true){
             
         }
